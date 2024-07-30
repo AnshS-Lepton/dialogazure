81e2b2e314ffdb1a5c964c036605fbe8018739c0
abhishek <abhishek.singh@leptonmaps.com>
2024-07-30 09:05:13 +0530

    fixed issue

--- Source/SmartInventory/SmartInventory/MigrationScripts/29-07-2024 - Customer_API.sql
+++ Source/SmartInventory/SmartInventory/MigrationScripts/29-07-2024 - Customer_API.sql
@@ -73,35 +73,4 @@ ALTER FUNCTION public.fn_save_customer_ticket(integer, character varying, charac
 
 
 
-CREATE OR REPLACE FUNCTION public.fn_get_customer_ticket_status(
-	p_ticket_id integer)
-    RETURNS TABLE(ticket_id text, ticket_status character varying, can_id text, created_on text, assigned_to text, assigned_date text, target_date text, remarks character varying) 
-    LANGUAGE 'plpgsql'
-    COST 100
-    VOLATILE PARALLEL UNSAFE
-    ROWS 1000
 
-AS $BODY$
-
-BEGIN
-    RETURN QUERY
-    SELECT 
-        tm.ticket_id ::text AS ticket_id,
-	   tm.ticket_status,
-        tm.can_id ::text AS can_id,
-		to_char(tm.created_on, 'DD-Mon-YY'::text) AS created_on,
-          um.user_name::text AS assigned_to,
-		to_char(tm.assigned_date, 'DD-Mon-YY'::text) AS assigned_date,
-		to_char(tm.target_date, 'DD-Mon-YY'::text) AS target_date,     
-        tm.remarks
-    FROM 
-        ticket_master tm
-		left join user_master as  um on tm.assigned_by=um.user_id 
-		left join ticket_status_master ttm ON tm.ticket_type_id=ttm.id 
-    WHERE 
-        tm.ticket_id = p_ticket_id;
-END;
-$BODY$;
-
-ALTER FUNCTION public.fn_get_customer_ticket_status(integer)
-    OWNER TO postgres;
