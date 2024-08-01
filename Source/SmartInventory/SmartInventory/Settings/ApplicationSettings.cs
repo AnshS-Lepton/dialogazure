7aebd342b7d155bd295ffb7b9395d84abd602a08
PawanK-Lepton <pawan.kumar@leptonsoftware.com>
2024-08-01 16:02:00 +0530

    icon change for CPF and site layer added in map files

--- Source/SmartInventory/SmartInventory/Settings/ApplicationSettings.cs
+++ Source/SmartInventory/SmartInventory/Settings/ApplicationSettings.cs
@@ -234,7 +234,7 @@ namespace SmartInventory.Settings
 		public static double MaxMeterReading = 0.0;
         public static int splitterTypeForFat = 0;
         public static int splitterTypeForFdc = 0;
-        public static bool IsWMSLayerLoadingEnabled = true;
+        public static bool IsWMSLayerLoadingEnabled = false;
         public static bool IsVectorLayerEnabled = false;
         public static int IsSignalRIsEnable = 0;
 		public static bool IsTraceEnabled = false;
@@ -854,10 +854,12 @@ namespace SmartInventory.Settings
                 if (objSetting.key == "IsWMSLayerLoadingEnabled")
                 {
                     IsWMSLayerLoadingEnabled = Convert.ToInt32(objSetting.value) == 0 ? false : true;
+                    //IsWMSLayerLoadingEnabled = true;
                 }
                 if (objSetting.key == "IsVectorLayerEnabled")
                 {
                     IsVectorLayerEnabled = Convert.ToInt32(objSetting.value) == 0 ? false : true;
+                   // IsVectorLayerEnabled = false;
                 }
                 if (objSetting.key == "IsSignalRIsEnable")
                 {
