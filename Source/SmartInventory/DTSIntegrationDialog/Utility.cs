43c7a112678b00dabdbb3ea7aef0b4b1754d9d4f
Chandra <chandra.sahni@leptonsoftware.com>
2024-11-22 10:53:12 +0530

    Implemented logging and handled exception for DateTime and Int field

---
+++ Source/SmartInventory/DTSIntegrationDialog/Utility.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTSIntegrationDialog
+{
+    public class CustomDateTimeConverter : JsonConverter<DateTime?>
+    {
+        public override void WriteJson(JsonWriter writer, DateTime? value, JsonSerializer serializer)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteValue(value.Value.ToString("o")); // Use the desired date format
+            }
+            else
+            {
+                writer.WriteNull();
+            }
+        }
+
+        public override DateTime? ReadJson(JsonReader reader, Type objectType, DateTime? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.String)
+            {
+                var dateStr = (string)reader.Value;
+                if (DateTime.TryParse(dateStr, out DateTime date))
+                {
+                    return date;
+                }
+                if (dateStr == "0")
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+
+}
