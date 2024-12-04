37201bfb701de6db6f51c8b9331b622f6a104611
Chandra <chandra.sahni@leptonsoftware.com>
2024-12-04 11:52:44 +0530

    Export Report - KML,AllExcel,Allshape not working #2689

---
+++ Source/SmartInventory/SmartInventory/MigrationScripts/2024_12_04_Issue_#2689_ExportReport.sql
@@ -0,0 +1,287 @@
+drop view vw_att_details_pod;
+-------------------------------------------------------------------------
+-- public.vw_att_details_pod source
+CREATE OR REPLACE VIEW public.vw_att_details_pod
+AS SELECT pod.system_id,
+    pod.network_id,
+    pod.pod_name,
+    round(pod.latitude::numeric, 6) AS latitude,
+    round(pod.longitude::numeric, 6) AS longitude,
+    pod.pincode,
+    pod.address,
+    pod.specification,
+    pod.category,
+    pod.subcategory1,
+    pod.subcategory2,
+    pod.subcategory3,
+    pod.item_code,
+    pod.vendor_id,
+    pod.type,
+    pod.brand,
+    pod.model,
+    pod.construction,
+    pod.activation,
+    pod.accessibility,
+    pod.status,
+    pod.network_status,
+    pod.province_id,
+    pod.region_id,
+    pod.created_by,
+    pod.created_on,
+    pod.modified_by,
+    pod.modified_on,
+    pod.parent_system_id,
+    pod.parent_network_id,
+    pod.parent_entity_type,
+    pod.sequence_id,
+    pod.structure_id,
+    pod.shaft_id,
+    pod.floor_id,
+    pod.project_id,
+    pod.planning_id,
+    pod.purpose_id,
+    pod.workorder_id,
+    prov.province_name,
+    reg.region_name,
+    um.user_name,
+    vm.name AS vendor_name,
+    tp.type AS pod_types,
+    bd.brand AS pod_brand,
+    ml.model AS pod_model,
+    fn_get_date(pod.created_on) AS created_date,
+    fn_get_date(pod.modified_on) AS modified_date,
+    um.user_name AS created_by_user,
+    um2.user_name AS modified_by_user,
+    ( SELECT system_spec_master.prop_name
+           FROM system_spec_master
+          WHERE system_spec_master.id = pod.accessibility) AS pod_accessibility,
+    ( SELECT system_spec_master.prop_name
+           FROM system_spec_master
+          WHERE system_spec_master.id = pod.construction) AS pod_construction,
+    ( SELECT system_spec_master.prop_name
+           FROM system_spec_master
+          WHERE system_spec_master.id = pod.activation) AS pod_activation,
+    ''::character varying AS additional_attributes,
+    ''::text AS total_ports,
+    ''::text AS entity_category,
+    pod.pod_name AS entity_name,
+    pod.acquire_from,
+    pod.pod_type,
+    pod.ownership_type,
+    vm2.id AS third_party_vendor_id,
+    vm2.name AS third_party_vendor_name,
+    pod.source_ref_type,
+    pod.source_ref_id,
+    pod.source_ref_description,
+    pod.status_remark,
+    pod.status_updated_by,
+    pod.status_updated_on,
+    pod.is_visible_on_map,
+    pod.is_new_entity,
+    pod.remarks,
+    pod.audit_item_master_id,
+    pod.is_acquire_from,
+    isp_room_info.network_id AS unit_network_id,
+    s.shaft_name,
+    f.floor_name,
+    pod.origin_from,
+    pod.origin_ref_id,
+    pod.origin_ref_code,
+    pod.origin_ref_description,
+    pod.request_ref_id,
+    pod.requested_by,
+    pod.request_approved_by,
+    pod.subarea_id,
+    pod.area_id,
+    pod.dsa_id,
+    pod.csa_id,
+    pod.bom_sub_category,
+    pod.gis_design_id,
+    pod.st_x,
+    pod.st_y,
+    pod.ne_id,
+    pod.prms_id,
+    pod.jc_id,
+    pod.mzone_id,
+    pod.hierarchy_type,
+    vm3.name AS own_vendor_name,
+    pod.site_id,
+    pod.site_name,
+    pod.on_air_date,
+    pod.removed_date,
+    pod.tx_type,
+    pod.tx_technology,
+    pod.tx_segment,
+    pod.tx_ring,
+    pod.province,
+    pod.district,
+    pod.depot,
+    pod.ds_division,
+    pod.local_authority,
+    pod.owner_name,
+    pod.access_24_7,
+    pod.tower_type,
+    pod.tower_height,
+    pod.cabinet_type,
+    pod.solution_type,
+    pod.site_rank,
+    pod.self_tx_traffic,
+    pod.agg_tx_traffic,
+    pod.csr_count,
+    pod.dti_circuit,
+    pod.agg_01,
+    pod.agg_02,
+    pod.bandwidth,
+    pod.ring_type,
+    pod.link_id,
+    pod.alias_name,
+    (pod.site_id::text || '-'::text) || pod.site_name::text AS site_id_site_name
+   FROM att_details_pod pod
+     JOIN province_boundary prov ON pod.province_id = prov.id
+     JOIN region_boundary reg ON pod.region_id = reg.id
+     JOIN user_master um ON pod.created_by = um.user_id
+     JOIN vendor_master vm ON pod.vendor_id = vm.id
+     LEFT JOIN vendor_master vm2 ON pod.third_party_vendor_id = vm2.id
+     LEFT JOIN vendor_master vm3 ON pod.own_vendor_id::integer = vm3.id
+     LEFT JOIN isp_type_master tp ON pod.type = tp.id
+     LEFT JOIN isp_brand_master bd ON pod.brand = bd.id
+     LEFT JOIN isp_base_model ml ON pod.model = ml.id
+     LEFT JOIN user_master um2 ON pod.modified_by = um2.user_id
+     LEFT JOIN ( SELECT isp_entity_mapping.entity_id,
+            isp_entity_mapping.parent_id
+           FROM isp_entity_mapping
+          WHERE isp_entity_mapping.entity_type::text = 'POD'::text) m1 ON pod.system_id = m1.entity_id
+     LEFT JOIN ( SELECT isp_entity_mapping.id,
+            isp_entity_mapping.structure_id,
+            isp_entity_mapping.shaft_id,
+            isp_entity_mapping.floor_id,
+            isp_entity_mapping.entity_type,
+            isp_entity_mapping.entity_id,
+            isp_entity_mapping.parent_id
+           FROM isp_entity_mapping
+          WHERE isp_entity_mapping.entity_type::text = 'UNIT'::text) m2 ON m1.parent_id = m2.id
+     LEFT JOIN isp_room_info ON m2.entity_id = isp_room_info.system_id AND m2.floor_id = isp_room_info.floor_id
+     LEFT JOIN isp_shaft_info s ON m2.shaft_id = s.system_id
+     LEFT JOIN isp_floor_info f ON m2.floor_id = f.system_id;
+-------------------------------------------------------------------------
+drop view vw_att_details_pod_report;
+-------------------------------------------------------------------------
+-- public.vw_att_details_pod_report source
+
+CREATE OR REPLACE VIEW public.vw_att_details_pod_report
+AS SELECT pod.system_id,
+    (pod.site_id::text || '-'::text) || pod.site_name::text AS site_id_site_name,
+    pod.network_id,
+    pod.pod_name,
+    round(pod.latitude::numeric, 6) AS latitude,
+    round(pod.longitude::numeric, 6) AS longitude,
+    pod.pincode,
+    pod.address,
+    pod.specification,
+    pod.category,
+    pod.subcategory1,
+    pod.subcategory2,
+    pod.subcategory3,
+    pod.item_code,
+        CASE
+            WHEN pod.network_status::text = 'P'::text THEN 'Planned'::text
+            WHEN pod.network_status::text = 'A'::text THEN 'As Built'::text
+            WHEN pod.network_status::text = 'D'::text THEN 'Dormant'::text
+            ELSE NULL::text
+        END AS network_status,
+    COALESCE(es.description, pod.status) AS status,
+    pod.parent_network_id AS parent_code,
+    pod.parent_entity_type AS parent_type,
+    prov.province_name,
+    reg.region_name,
+    vm.name AS vendor_name,
+    pm.project_code,
+    pm.project_name,
+    plningm.planning_code,
+    plningm.planning_name,
+    workorm.workorder_code,
+    workorm.workorder_name,
+    purposem.purpose_code,
+    purposem.purpose_name,
+    to_char(pod.created_on, 'DD-Mon-YY'::text) AS created_on,
+    um.user_name AS created_by,
+    to_char(pod.modified_on, 'DD-Mon-YY'::text) AS modified_on,
+    um2.user_name AS modified_by,
+    pod.region_id,
+    pod.province_id,
+    pm.system_id AS project_id,
+    plningm.system_id AS planning_id,
+    workorm.system_id AS workorder_id,
+    purposem.system_id AS purpose_id,
+    um.user_id AS created_by_id,
+    pod.acquire_from,
+    pod.pod_type,
+    pod.ownership_type,
+    vm2.name AS third_party_vendor_name,
+    vm2.id AS third_party_vendor_id,
+    pod.source_ref_type,
+    pod.source_ref_id,
+    pod.source_ref_description,
+    pod.status_remark,
+    pod.status_updated_by,
+    to_char(pod.status_updated_on, 'DD-Mon-YY'::text) AS status_updated_on,
+    entattr.erp_code,
+    entattr.erp_name,
+    entattr.ef_customers,
+    entattr.status AS locked,
+    entattr.zone,
+    to_char(entattr.rfs_date, 'DD-Mon-YY'::text) AS rfs_date,
+    entattr.hub_maintained,
+    entattr.hm_power_bb,
+    entattr.hm_earthing_rating,
+    entattr.hm_rack,
+    entattr.hm_olt_bb,
+    entattr.hm_fms,
+    entattr.splicing_machine,
+    entattr.optical_power_meter,
+    entattr.otdr,
+    entattr.laptop_with_giga_port,
+    entattr.l3_updation_on_inms,
+    pod.is_visible_on_map,
+    pod.remarks,
+    reg.country_name,
+    pod.origin_from,
+    pod.origin_ref_id,
+    pod.origin_ref_code,
+    pod.origin_ref_description,
+    pod.request_ref_id,
+    pod.requested_by,
+    pod.request_approved_by,
+    pod.subarea_id,
+    pod.area_id,
+    pod.dsa_id,
+    pod.csa_id,
+    pod.bom_sub_category,
+    pod.gis_design_id AS design_id,
+    pod.st_x,
+    pod.st_y,
+    pod.ne_id,
+    pod.prms_id,
+    pod.jc_id,
+    pod.mzone_id,
+    pod.served_by_ring,
+    pod.hierarchy_type,
+    vm3.name AS own_vendor_name,
+    pod.site_id,
+    pod.site_name
+   FROM att_details_pod pod
+     JOIN province_boundary prov ON pod.province_id = prov.id
+     JOIN region_boundary reg ON pod.region_id = reg.id
+     JOIN user_master um ON pod.created_by = um.user_id
+     JOIN vendor_master vm ON pod.vendor_id = vm.id
+     LEFT JOIN entity_additional_attributes entattr ON entattr.system_id = pod.system_id AND upper(entattr.entity_type::text) = upper('POD'::text)
+     LEFT JOIN vendor_master vm2 ON pod.third_party_vendor_id = vm2.id
+     LEFT JOIN vendor_master vm3 ON pod.own_vendor_id::integer = vm3.id
+     LEFT JOIN att_details_project_master pm ON pod.project_id = pm.system_id
+     LEFT JOIN att_details_planning_master plningm ON pod.planning_id = plningm.system_id
+     LEFT JOIN att_details_workorder_master workorm ON pod.workorder_id = workorm.system_id
+     LEFT JOIN att_details_purpose_master purposem ON pod.purpose_id = purposem.system_id
+     LEFT JOIN user_master um2 ON pod.modified_by = um2.user_id
+     LEFT JOIN entity_status_master es ON es.status::text = pod.status::text;
+-------------------------------------------------------------------------
+select * from  fn_sync_layer_columns();
