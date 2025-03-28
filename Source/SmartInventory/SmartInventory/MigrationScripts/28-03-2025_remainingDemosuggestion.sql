f7f8231b01aca7cd93c7d48c46edc7664fbbfaf5
DeepS-Lepton <deep.shukla@leptonsoftware.com>
2025-03-28 17:36:24 +0530

    update remaining work

---
+++ Source/SmartInventory/SmartInventory/MigrationScripts/28-03-2025_remainingDemosuggestion.sql
@@ -0,0 +1,8 @@
+
+update res_resources set value='Site ID' where key='SI_OSP_GBL_NET_RPT_357'  and value='Site Id';
+
+update res_resources set value='Aggregate Site 1' where key='SI_OSP_GBL_NET_RPT_459'  and value='AGG1';
+
+update res_resources set value='Aggregate Site 2' where key='SI_OSP_GBL_NET_RPT_460' and value='AGG2';
+
+update res_resources set value='Max. Sites per Ring' where key='SI_OSP_GBL_NET_RPT_461' and value='No.Of Sites';
